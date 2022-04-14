1e454cd84fd252131cf59cad032bc61a4aed3718
António Fernandes <antoniofernandes@MBP-de-Antonio.Home>
2022-04-14 16:29:37 +0100

    create components and apply page style

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Header.scss';
+
+function Header() {
+  return (
+    <header className="header">
+      <span className="header__title">Schedules</span>
+      <span className="header__navigation">Navigation</span>
+    </header>
+  );
+}
+
+export default Header;
--- src/containers/Page.tsx
+++ src/containers/Page.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import Header from '../components/Header/Header';
+import MainPage from '../components/MainPage/MainPage';
+import SideBar from '../components/SideBar/SideBar';
+import './Page.scss';
 
 function Page() {
   return (
-    <>
-    </>
+    <div className="page">
+      <Header />
+      <SideBar />
+      <MainPage />
+    </div>
   );
 }
 
